59cc79767096687c04c0e5016e1746bb6adec3ae
xixizhiisme <1056424198@qq.com>
2019-04-24 21:29:50 +0800

    4.24

--- app/src/main/java/com/linzhi/myapp/RateActivity.java
+++ app/src/main/java/com/linzhi/myapp/RateActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +15,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class RateActivity extends Activity implements Runnable{
     private final String TAG = "Rate";
     private float rateDollar=1/6.7f;
@@ -22,6 +33,7 @@ public class RateActivity extends Activity implements Runnable{
 
     EditText rmb;
     TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +58,24 @@ public class RateActivity extends Activity implements Runnable{
         Thread t=new Thread(this);
         t.start();
 
-        Handler handler=new Handler();
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what==5){
+                    Bundle bdl=(Bundle) msg.obj;
+                    rateDollar=bdl.getFloat("rate_dollar");
+                    rateEuro=bdl.getFloat("rate_euro");
+                    rateWon=bdl.getFloat("rate_won");
+                    Log.i(TAG, "handleMessage: dollar:"+rateDollar);
+                    Log.i(TAG, "handleMessage: euro:"+rateEuro);
+                    Log.i(TAG, "handleMessage: won:"+rateWon);
+                    Toast.makeText(RateActivity.this,"汇率已更新",Toast.LENGTH_SHORT).show();
+                    /*Log.i(TAG, "handleMessage: getMessage msg="+str);
+                    show.setText(str);*/
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
 
     public void onClick(View btn){
@@ -124,5 +153,85 @@ public class RateActivity extends Activity implements Runnable{
     @Override
     public void run() {
         Log.i(TAG, "run: run()……");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //用于保存获取的汇率
+        Bundle bundle=new Bundle();
+        //获取msg对象，用于返回主线程
+        Message msg=handler.obtainMessage(5);
+        //msg.what=5;
+        //msg.obj="Hello from run()";
+        msg.obj=bundle;
+        handler.sendMessage(msg);
+        //获取网络数据
+        /*URL url= null;
+        try {
+            url = new URL("http://www.usd-cny.com/bankofchina.htm");
+            HttpURLConnection http= (HttpURLConnection) url.openConnection();
+            InputStream in=http.getInputStream();
+            String html=inputStream2String(in);
+            Log.i(TAG, "run: html="+html);
+            Document doc=Jsoup.parse(html);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
+            Log.i(TAG, "run: "+doc.title());
+            Elements tables=doc.getElementsByTag("table");
+            /*for(Element table:tables){
+                Log.i(TAG, "run: table["+i+"]="+table);
+                i++;
+            }*/
+            Element table6=tables.get(0);
+            //Log.i(TAG, "run: table6="+table6);
+            //获取TD中的数据
+            Elements tds=table6.getElementsByTag("td");
+            for(int i=0;i<tds.size();i+=6){
+                Element td1=tds.get(i);
+                Element td2=tds.get(i+5);
+                Log.i(TAG, "run: "+td1.text()+"==>"+td2.text());
+                String str1=td1.text();
+                String val=td2.text();
+                if("美元".equals(str1)){
+                    bundle.putFloat("rate_dollar",100f/Float.parseFloat(val));
+                }else if("欧元".equals(str1)){
+                    bundle.putFloat("rate_euro",100f/Float.parseFloat(val));
+                }else if("韩元".equals(str1)){
+                    bundle.putFloat("rate_won",100f/Float.parseFloat(val));
+                }
+            }
+            /*for(Element td:tds){
+                Log.i(TAG, "run: td="+td);
+                Log.i(TAG, "run: text="+td.text());
+                Log.i(TAG, "run: html="+td.html());
+            }*/
+            /*Elements newsHeadlines = doc.select("#mp-itn b a");
+            for (Element headline : newsHeadlines) {
+                Log.i(TAG, "%s\n\t%s" + headline.attr("title") + headline.absUrl("href"));
+            }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //bundle中保存获取的汇率
+    }
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer=new char[bufferSize];
+        final StringBuilder out=new StringBuilder();
+        Reader in = new InputStreamReader(inputStream,"gb2312");
+        for(;;){
+            int rsz=((InputStreamReader) in).read(buffer,0,buffer.length);
+            if(rsz<0)
+                break;
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
